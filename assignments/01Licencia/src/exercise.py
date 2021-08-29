b6902c2d23bb442c111192304c739c6806b0c8b3
A01284609 <A01284609@itesm.mx>
2021-08-29 18:52:22 -0500

    29/08/2021

--- assignments/01Licencia/src/exercise.py
+++ assignments/01Licencia/src/exercise.py
@@ -1,7 +1,18 @@
-
 def main():
-    pass
 
+    edad = int(input ("Ingresa tu edad: "))
+    if edad >= 18 :
+        id = (input("¿Tienes identificación oficial? (s/n): "))
+        if  id == "s":
+            print ("Trámite de licencia concedido")
+        elif id == "n" :
+            print ("No cumples los requisitos")
+        else :
+            print ("Respuesta incorrecta")
+    elif (edad < 18 and edad >=0):
+        print ("No cumples los requisitos")
+    else :
+        print ("Respuesta incorrecta")
 
 if __name__ == '__main__':
     main()
--- assignments/02Ordena/src/exercise.py
+++ assignments/02Ordena/src/exercise.py
@@ -1,10 +1,38 @@
 def main():
-    # Escribe el código adecuado para completar el programa
+
     x = int(input("Ingresa el primer número: "))
     y = int(input("Ingresa el segundo número: "))
     z = int(input("Ingresa el tercer número: "))
-    
-    print("Hola")
+
+    if (x >= y) and (x <= z):
+        print(y)
+        print(x)
+        print(z)
+
+    elif (x <= y) and (x >= z):
+        print(z)
+        print(x)
+        print(y)
+
+    elif (y >= z) and (y <= x):
+        print(z)
+        print(y)
+        print(x)
+
+    elif (y > x) and (y <= z):
+        print(x)
+        print(y)
+        print(z)
+
+    elif (z > y) and (z < x):
+        print(y)
+        print(z)
+        print(x)
+        
+    elif (z > x) and (z < y):
+        print(x)
+        print(z)
+        print(y)
 
 if __name__=='__main__':
     main()
